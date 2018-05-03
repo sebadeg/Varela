08ce41fe2f8589a3a1d99252469d18820d404f9f
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2018-05-02 23:19:11 -0300

    Nuevo

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -13,6 +13,7 @@ ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.irregular 'cuenta', 'cuentas'
   inflect.irregular 'titular_cuenta', 'titular_cuentas'
   inflect.irregular 'actividad', 'actividades'
+  inflect.irregular 'actividad_opcion', 'actividad_opciones'
 end
 
 # These inflection rules are supported but not enabled by default:
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423142608) do
+ActiveRecord::Schema.define(version: 20180503014048) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,10 +25,22 @@ ActiveRecord::Schema.define(version: 20180423142608) do
     t.index ["alumno_id"], name: "index_actividad_alumnos_on_alumno_id", using: :btree
   end
 
+  create_table "actividad_opciones", force: :cascade do |t|
+    t.integer  "actividad_id"
+    t.integer  "valor"
+    t.string   "opcion"
+    t.string   "eleccion"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["actividad_id"], name: "index_actividad_opciones_on_actividad_id", using: :btree
+  end
+
   create_table "actividades", force: :cascade do |t|
     t.string   "nombre"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "descripcion"
+    t.string   "archivo"
   end
 
   create_table "alumnos", force: :cascade do |t|
@@ -108,13 +120,9 @@ ActiveRecord::Schema.define(version: 20180423142608) do
     t.string   "alumno2"
     t.string   "alumno3"
     t.boolean  "validado"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.integer  "failed_attempts",        default: 0,  null: false
-    t.string   "unlock_token"
-    t.datetime "locked_at"
+    t.integer  "cuenta"
+    t.string   "password"
+    t.boolean  "mail"
     t.index ["cedula"], name: "index_usuarios_on_cedula", unique: true, using: :btree
     t.index ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
@@ -122,6 +130,7 @@ ActiveRecord::Schema.define(version: 20180423142608) do
 
   add_foreign_key "actividad_alumnos", "actividades"
   add_foreign_key "actividad_alumnos", "alumnos"
+  add_foreign_key "actividad_opciones", "actividades"
   add_foreign_key "cuenta_alumnos", "alumnos"
   add_foreign_key "cuenta_alumnos", "cuentas"
   add_foreign_key "grado_alumnos", "alumnos"
