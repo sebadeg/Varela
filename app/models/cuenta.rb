6032ed0f2fa1837dab1df3f55f162eb4951a5c6a
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-03-18 20:34:24 -0300

    Cuentas

--- app/models/cuenta.rb
+++ app/models/cuenta.rb
@@ -1,28 +1,16 @@
 class Cuenta < ApplicationRecord
 
 	def movimientos
-		sql=
-			"SELECT * FROM movimientos WHERE NOT pendiente AND cuenta_id = #{id} ORDER BY fecha, tipo";
+		#sql= "SELECT * FROM movimientos WHERE NOT pendiente AND cuenta_id = #{id} ORDER BY fecha, tipo";
 
-		# sql=
-		# 	"SELECT * FROM " +
-		# 	"(SELECT facturas.cuenta_id as cuenta_id, facturas.fecha as fecha, linea_facturas.indice as indice, linea_facturas.descripcion as descripcion, linea_facturas.importe as debe, 0 as haber FROM linea_facturas INNER JOIN facturas ON linea_facturas.factura_id=facturas.id " +
-		# 	"WHERE saldo AND cuenta_id = #{id} " +
-		# 	"UNION " +
-		# 	"SELECT cuenta_id, fecha, 1000 as indice,'Pago' as descripcion,0 as debe,importe as haber FROM pago_cuentas " +
-		# 	"WHERE cuenta_id = #{id} " +
-		# 	"UNION " +
-		# 	"SELECT cuenta_id, fecha, 1000 as indice,'Pago' as descripcion,0 as debe,haber as haber FROM movimientos WHERE cuenta_id = #{id} AND descripcion='Pago' AND pago_cuenta_id IS NULL ) AS movimientos " +
-		# 	"ORDER BY cuenta_id, fecha, indice";
-
-      	return ActiveRecord::Base.connection.execute(sql)
+      	return Movimiento.where(["fecha<=? AND cuenta_id=?",DateTime.now,id).order(:fecha,:tipo)
+      	#return ActiveRecord::Base.connection.execute(sql)
 
 	end
 
 
     def movimientos_pendientes
-		sql=
-			"SELECT * FROM movimientos WHERE pendiente AND cuenta_id = #{id} ORDER BY fecha, tipo";
+		#sql= "SELECT * FROM movimientos WHERE pendiente AND cuenta_id = #{id} ORDER BY fecha, tipo";
 
 		# sql=
 		# 	"SELECT * FROM " +
@@ -35,7 +23,8 @@ class Cuenta < ApplicationRecord
 		# 	"SELECT cuenta_id, fecha, 1000 as indice,'Pago' as descripcion,0 as debe,haber as haber FROM movimientos WHERE cuenta_id = #{id} AND descripcion='Pago' AND pago_cuenta_id IS NULL ) AS movimientos " +
 		# 	"ORDER BY cuenta_id, fecha, indice";
 
-      	return ActiveRecord::Base.connection.execute(sql)
+      	return Movimiento.where(["fecha>? AND cuenta_id=?",DateTime.now,id).order(:fecha,:tipo)
+      	#return ActiveRecord::Base.connection.execute(sql)
 
 	end
 end
