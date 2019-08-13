e23abc09d75078ba2df6fe8973bd218baf3b5259
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-13 20:27:26 -0300

    Insc

--- app/models/formulario.rb
+++ app/models/formulario.rb
@@ -1,33 +1,39 @@
 class Formulario < ApplicationRecord
 
   def self.OpcionesFormulario(inscripcionAlumno)
-  	opciones = Array.new
-  	opcionesConvenio.push( ["Prueba 1",1] )
+    opciones = Array.new
+    opciones.push( ["Prueba 1",1] )
+    return opciones
   end
 
   def self.OpcionesConvenio(inscripcionAlumno)
-  	opciones = Array.new
-  	opcionesConvenio.push( ["Prueba 1",1] )
+    opciones = Array.new
+    opciones.push( ["Prueba 1",1] )
+    return opciones
   end
 
   def self.OpcionesAdicional(inscripcionAlumno)
-  	opciones = Array.new
-  	opcionesConvenio.push( ["Prueba 1",1] )
+    opciones = Array.new
+    opciones.push( ["Prueba 1",1] )
+    return opciones
   end
 
   def self.OpcionesHermanos(inscripcionAlumno)
-  	opciones = Array.new
-  	opcionesConvenio.push( ["Prueba 1",1] )
+    opciones = Array.new
+    opciones.push( ["Prueba 1",1] )
+    return opciones
   end
 
   def self.OpcionesCuotas(inscripcionAlumno)
-  	opciones = Array.new
-  	opcionesConvenio.push( ["Prueba 1",1] )
+    opciones = Array.new
+    opciones.push( ["Prueba 1",1] )
+    return opciones
   end
 
   def self.OpcionesMatricula(inscripcionAlumno)
-  	opciones = Array.new
-  	opcionesConvenio.push( ["Prueba 1",1] )
+    opciones = Array.new
+    opciones.push( ["Prueba 1",1] )
+    return opciones
   end
 
 end
--- app/models/proximo_grado.rb
+++ app/models/proximo_grado.rb
@@ -3,6 +3,7 @@ class ProximoGrado < ApplicationRecord
   def self.OpcionesGrados(inscripcionAlumno)
   	opciones = Array.new
   	opciones.push( ["Prueba 1",1] )
+  	return opciones
   end
 
 end
