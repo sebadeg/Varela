33b2f50c2094c3a4e2d197491eae71d7168b8c50
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-13 21:42:49 -0300

    Insc

--- app/models/inscripcion.rb
+++ app/models/inscripcion.rb
@@ -2,7 +2,7 @@ class Inscripcion < ApplicationRecord
   belongs_to :convenio
   belongs_to :proximo_grado
 
-  #attr_accessor :cedula1, :cedula2
+  attr_accessor :nombre, :apellido, :lugar_nacimiento, :fecha_nacimiento, :domicilio, :celular, :mutualista, :emergencia
 
   def self.FindInscripcion(alumno)
     return Inscripcion.where("alumno_id=#{alumno} AND reinscripcion AND anio IN (SELECT anio_inscripciones FROM configs WHERE NOT anio_inscripciones IS NULL)").first rescue nil
