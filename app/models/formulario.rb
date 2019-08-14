a84316eb1f19222dfef755f53dfcff8dabb5e3d5
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-14 18:53:26 -0300

    Insc

--- app/models/formulario.rb
+++ app/models/formulario.rb
@@ -1,21 +1,22 @@
 class Formulario < ApplicationRecord
 
   def self.OpcionesFormulario(inscripcionAlumno)
-
     opciones = Array.new
-    tipo = InscripcionOpcionTipo.where("nombre='Formulario'").first rescue nil
+    Formulario.where("cedula=#{inscripcionAlumno.cedula} AND anio IN (SELECT anio_inscripciones FROM configs WHERE NOT anio_inscripciones IS NULL)").each do |formulario|
+      opciones.push( [formulario.nombre,formulario.id] )
+    end
+    return opciones
+  end
+
+  def self.OpcionesConvenio(inscripcionAlumno)
+    opciones = Array.new
+    tipo = InscripcionOpcionTipo.where("nombre='Convenio'").first rescue nil
     if tipo == nil 
       return opciones
     end
     InscripcionOpcion.where("inscripcion_opcion_tipo_id=#{tipo.id} AND general").each do |opcion|
       opciones.push( [opcion.nombre,opcion.id] )
     end 
-    return opciones
-  end
-
-  def self.OpcionesConvenio(inscripcionAlumno)
-    opciones = Array.new
-    opciones.push( ["Prueba 1",1] )
     return opciones
   end
 
