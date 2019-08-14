58a3f65836d0c3ecfea1c8cc2151a4fb58fc947d
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-14 19:48:29 -0300

    Insc

--- app/models/proximo_grado.rb
+++ app/models/proximo_grado.rb
@@ -2,7 +2,7 @@ class ProximoGrado < ApplicationRecord
 
   def self.OpcionesGrados(inscripcionAlumno)
   	opciones = Array.new
-    tipo = ProximoGrado.where("anio IN (SELECT anio_inscripciones FROM configs WHERE NOT anio_inscripciones IS NULL)").order(:nombre).each do |opcion|
+    tipo = ProximoGrado.where("grado_id=#{inscripcionAlumno.grado_id} AND anio IN (SELECT anio_inscripciones FROM configs WHERE NOT anio_inscripciones IS NULL)").order(:nombre).each do |opcion|
       opciones.push( [opcion.nombre,opcion.id] )
     end 
   	return opciones
