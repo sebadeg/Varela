266a6c71f632b105d54e7eecf770278deb42bd20
Sebastian Degrandi <sebastian.degrandi@gmail.com>
2019-08-13 23:34:11 -0300

    Insc

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,2 +1,11 @@
 class Persona < ApplicationRecord
+
+  def self.Find(p)
+    persona = Persona.find(p) rescue nil
+    if persona == nil
+      persona = Persona.new
+    end
+    return persona
+  end
+
 end
