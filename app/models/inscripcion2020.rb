2f7e729d895a1e93d79c4c876b1ac99b9d1f757c
Sebastian Degrandi <sdegrandi@practia.global>
2020-10-06 12:22:24 -0300

    insc

--- app/models/inscripcion2020.rb
+++ app/models/inscripcion2020.rb
@@ -1,17 +1,5 @@
 class Inscripcion2020 < ApplicationRecord
-  belongs_to :alumno
-  belongs_to :padre, :class_name => "Usuario"
-  belongs_to :madre, :class_name => "Usuario"
-  belongs_to :titular1, :class_name => "Usuario"
-  belongs_to :titular2, :class_name => "Usuario"
-  belongs_to :grado
   belongs_to :proximo_grado
-  belongs_to :formulario2020
-  belongs_to :convenio2020
-  belongs_to :afinidad2020
-  belongs_to :matricula2020
-  belongs_to :hermanos2020
-  belongs_to :cuota2020
 
   def self.FindInscripcion(a)
     return Inscripcion2020.where("alumno_id=#{a} AND reinscripcion AND #{ConsultaFecha()}").first rescue nil
